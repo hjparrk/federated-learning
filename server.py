15efa683377863c2b71047baa28c14d2fba3fe47
Hyojoon Park <hpar9758@naver.com>
2024-04-15 16:58:48 +1000

    refactor server.py

--- server.py
+++ server.py
@@ -5,193 +5,170 @@ import select
 import socket
 import threading
 
-import torch
-import torch.nn as nn
-
-
-class LinearRegressionModel(nn.Module):
-    def __init__(self, input_size=1):
-        super(LinearRegressionModel, self).__init__()
-        # Create a linear transformation to the incoming data
-        self.linear = nn.Linear(input_size, 1)
-
-    # Define how the model is going to be run, from input to output
-    def forward(self, x):
-        # Apply linear transformation
-        output = self.linear(x)
-        return output.reshape(-1)
-
 
 class Server():
+
     def __init__(self):
         # init
         self.clients = []
         self.host = "127.0.0.1"
         self.port = int(sys.argv[1])
         self.num_subsamples = 5 if int(sys.argv[2]) == 0 else int(sys.argv[2])
-        self.server_model = LinearRegressionModel(8)
+        self.server_model = []
         self.batch_size = 64
         self.learning_rate = 0.01
         self.num_glob_iters = 10  # No. of global rounds
         self.total_train_samples = 0
 
+    def calculate_total_train_samples(self):
+        # calculate total_train_samples
+        self.total_train_samples = 0
+        for client in self.clients:
+            self.total_train_samples += client["train_samples"]
 
-def calculate_total_train_samples(clients):
-    # calculate total_train_samples
-    total_train_samples = 0
-    for client in clients:
-        total_train_samples += client["train_samples"]
+    def register_client(self, conn):
 
-    return total_train_samples
+        try:
+            # receive and decode registration info
+            recv = b'' + conn.recv(1024)
+            decoded = json.loads(recv.decode("utf-8"))
 
+            # register client
+            self.clients.append(decoded)
 
-def register_client(conn, clients):
+            # close the connection
+            conn.close()
 
-    try:
-        # receive and decode registration info
-        recv = b'' + conn.recv(1024)
-        decoded = json.loads(recv.decode("utf-8"))
+        except Exception as e:
+            print(f"Error: {e}")
 
-        # register client
-        clients.append(decoded)
+    def run_init_phase(self):
+        # create a TCP socket server
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind((self.host, self.port))
+        server.listen(5)
 
-        # close the connection
-        conn.close()
-
-    except Exception as e:
-        print(f"Error: {e}")
-
-
-def run_init_phase(host, port, clients):
-    # create a TCP socket server
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((host, port))
-    server.listen(5)
-
-    # first client tries to register
-    try:
-        conn, addr = server.accept()
-        threading.Thread(target=register_client,
-                         args=(conn, clients)).start()
-    except Exception as e:
-        print("Error: {e}")
-
-    # wait 30 seconds for other clients to register
-    start_time = time.time()
-    while time.time() - start_time < 30:
-        server.settimeout(30 - (time.time() - start_time))
+        # first client tries to register
         try:
             conn, addr = server.accept()
-            threading.Thread(target=register_client,
-                             args=(conn, clients)).start()
-        except socket.timeout:
-            server.close()
-            break
+            threading.Thread(target=self.register_client,
+                             args=(conn,)).start()
+        except Exception as e:
+            print("Error: {e}")
 
-    server.close()
-
-
-def broadcast_model(server: Server, client):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-
-        # connect to client
-        host = server.host
-        port = int(client["client_port"])
-        server_socket.connect((host, port))
-
-        data = {"model": "server"}
-        encoded = json.dumps(data).encode("utf-8")
-
-        client_id = client["client_id"]
-        print(f"send model to {client_id}")
-        # send server model
-        server_socket.send(encoded)
-
-        recv = b'' + server_socket.recv(1024)
-        decoded = json.loads(recv.decode("utf-8"))
-
-        local_model = decoded["model"]
-        print(f"receive {local_model} model")
-
-
-def run_epoch(server: Server):
-
-    threads = []
-
-    # create threads
-    for client in server.clients:
-        th = threading.Thread(target=(broadcast_model),
-                              args=(server, client, ))
-
-        # store thread to list
-        threads.append(th)
-
-    # start threads
-    for th in threads:
-        th.start()
-
-    # wait for all threads to finish the task
-    for th in threads:
-        th.join()
-
-
-def run_server(server: Server):
-
-    # run non-blocking server socket
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((server.host, server.port))
-    server_socket.listen(20)
-    server_socket.setblocking(False)
-
-    inputs = [server_socket]
-
-    for i in range(10):
-        print(f"\nepoch {i}")
-        # broadcast server model, receive local model, then aggregate
-        run_epoch(server)
-
-        # check for new client registration
-        while True:
-
-            # monitor readable sockets
-            readable, writable, errored = select.select(inputs, [], [], 0)
-
-            # break if there are no new socket connections
-            if not readable:
+        # wait 30 seconds for other clients to register
+        start_time = time.time()
+        while time.time() - start_time < 15:
+            server.settimeout(30 - (time.time() - start_time))
+            try:
+                conn, addr = server.accept()
+                threading.Thread(target=self.register_client,
+                                 args=(conn,)).start()
+            except socket.timeout:
+                server.close()
                 break
 
-            for s in readable:
+        server.close()
 
-                if s is server_socket:
+    def broadcast_model(self, client):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
 
-                    # connection from a new client
-                    conn, address = s.accept()
-                    conn.setblocking(False)
-                    inputs.append(conn)
+            # connect to client
+            host = self.host
+            port = int(client["client_port"])
+            server_socket.connect((host, port))
 
-                else:
-                    # receive registration data from the new client
-                    recv = conn.recv(1024)
+            data = {"model": "server"}
+            encoded = json.dumps(data).encode("utf-8")
 
-                    # data exists
-                    if recv:
-                        byted_recv = b'' + recv
-                        decoded = json.loads(byted_recv.decode("utf-8"))
+            client_id = client["client_id"]
+            print(f"send model to {client_id}")
+            # send server model
+            server_socket.send(encoded)
 
-                        # register new client
-                        server.clients.append(decoded)
-                        server.total_train_samples = calculate_total_train_samples(
-                            server.clients)
+            recv = b'' + server_socket.recv(1024)
+            decoded = json.loads(recv.decode("utf-8"))
 
-                        # close connection
-                        inputs.remove(s)
-                        s.close()
+            local_model = decoded["model"]
+            print(f"receive {local_model} model")
 
-                    # no data received
+    def run_epoch(self):
+
+        threads = []
+
+        # create threads
+        for client in self.clients:
+            th = threading.Thread(target=(self.broadcast_model),
+                                  args=(client, ))
+
+            # store thread to list
+            threads.append(th)
+
+        # start threads
+        for th in threads:
+            th.start()
+
+        # wait for all threads to finish the task
+        for th in threads:
+            th.join()
+
+    def run(self):
+
+        # run non-blocking server socket
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind((self.host, self.port))
+        server_socket.listen(20)
+        server_socket.setblocking(False)
+
+        inputs = [server_socket]
+
+        for i in range(10):
+            print(f"\nepoch {i}")
+
+            # broadcast server model, receive local model, then aggregate
+            self.run_epoch()
+
+            # check for new client registration
+            while True:
+
+                # monitor readable sockets
+                readable, writable, errored = select.select(inputs, [], [], 0)
+
+                # break if there are no new socket connections
+                if not readable:
+                    break
+
+                for s in readable:
+
+                    if s is server_socket:
+
+                        # connection from a new client
+                        conn, address = s.accept()
+                        conn.setblocking(False)
+                        inputs.append(conn)
+
                     else:
-                        # close connection
-                        inputs.remove(s)
-                        s.close()
+                        # receive registration data from the new client
+                        recv = conn.recv(1024)
+
+                        # data exists
+                        if recv:
+                            byted_recv = b'' + recv
+                            decoded = json.loads(byted_recv.decode("utf-8"))
+
+                            # register new client
+                            self.clients.append(decoded)
+                            self.calculate_total_train_samples()
+
+                            # close connection
+                            inputs.remove(s)
+                            s.close()
+
+                        # no data received
+                        else:
+                            # close connection
+                            inputs.remove(s)
+                            s.close()
 
 
 def main():
@@ -199,12 +176,17 @@ def main():
     # initialisation of server
     server = Server()
 
+    server.run_init_phase()
+    server.calculate_total_train_samples()
+
+    server.run()
+
     # initial clients registration phase
-    run_init_phase(server.host, server.port, server.clients)
-    server.total_train_samples = calculate_total_train_samples(server.clients)
+    # run_init_phase(server.host, server.port, server.clients)
+    # server.total_train_samples = calculate_total_train_samples(server.clients)
 
     # run algorithm
-    run_server(server)
+    # run_server(server)
 
 
 if __name__ == "__main__":
